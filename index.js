12bd623ceeac73d7421d0a0f579cc427fab880ae
Frank Pinto <frank@ayalo.co>
2015-05-04 13:40:39 -0600

    Bug fixes aaand its working

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 fs = require('fs');
+path = require('path');
 request = require('request');
 logger = require('winston');
 
 var main_file = process.argv[1];
-var root_dir = path.dirname(main_file);
+var root_dir = path.dirname(main_file) + '/';
 
 /* Downloader module does all the work */
 var downloader = {
   downloadPage: function(url, filename, callback) {
-    // Create place to save page
-    var out = root_dir + '/tmp/' + filename;
+    var cache_directory = 'tmp/';
 
-    // If cache file already exists, leave it
+    if (!fs.existsSync(cache_directory))
+      fs.mkdirSync(cache_directory);
+
+    // Create place to save page
+    var out = root_dir + cache_directory + filename;
+
+    // If cache file already exists return it
     var alreadyDownloaded = function() {
       if (fs.existsSync(out))
+      {
         return true;
+      }
       else
         return false;
     };
@@ -28,11 +36,12 @@ var downloader = {
     }
 
     // If doesn't, download page and save it!
+    var parent_object = this;
     request(url, function (error, response, body) {
-      this.saveFile(error, response, body);
+      parent_object.saveFile(error, response, out, body, callback);
     });
   },
-  saveFile: function(err, response, body) {
+  saveFile: function(err, response, out, body, callback) {
     // Inform user of filename
     logger.info('Attempting to write file ' + out);
 
@@ -64,16 +73,16 @@ var downloader = {
 
 /* Small helper function */
 var url_to_filename = function(url) {
-  return url.replace(/http:\/\/(.*)\.com/, '$1') + '.html';
+  return url.replace(/http:\/\/(www\.)?(.*)\.com\//, '$2') + '.html';
 };
 
 
 /* This is where the magic happens */
-pages_to_download = ['http://google.com/', 'http://yahoo.com', 'http://www.msn.com'];
+pages_to_download = ['http://google.com/', 'http://yahoo.com/', 'http://www.msn.com/'];
 
 pages_to_download.forEach(function(page) {
-  downloadPage(page, url_to_filename(page), function(err, body) {
-    console.log('Uncomment the line below this one to output the body of the page everytime it is retrieved');
-    //console.log(body);
+  downloader.downloadPage(page, url_to_filename(page), function(err, body) {
+    logger.info('Uncomment the line below this one to output the body of the page everytime it is retrieved');
+    //logger.info(body);
   });
 });
